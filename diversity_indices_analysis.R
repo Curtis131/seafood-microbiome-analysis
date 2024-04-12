8848c46ef003b31a128d76add794157bb8920ad3
Yishuo <yishuo.zhang@mail.mcgill.ca>
2024-04-12 12:04:59 -0400

    take mean of replicated sample, perform anova & plots

--- diversity_indices_analysis.R
+++ diversity_indices_analysis.R
@@ -1,3 +1,8 @@
+library(ggplot2)
+library(dplyr)
+
+
+combined_indices <- read.csv("combined_diversity_indices.csv", row.names = 1)
 sample_meta <- read.csv("merge.csv", row.names = 1)
 sample_meta <- sample_meta[sample_meta$treatment %in%
                              c("control","Nisin","Pediocin","Divergicin","MicrocinJ25"),]
@@ -6,16 +11,47 @@ sample_meta <- sample_meta[sample_meta$treatment %in%
 combined_indices$time <- sample_meta$time.day.
 combined_indices$treatment <- sample_meta$treatment
 combined_indices$treatment <- factor(combined_indices$treatment)
+combined_indices$time <- factor(combined_indices$time)
 
-# anova
+# take average of replicates
+combined_indices <- combined_indices %>%
+  group_by(time,treatment) %>%
+  summarise(
+    Chao1 = mean(Chao1),
+    Shannon = mean(Shannon),
+    Simpson = mean(Simpson),
+    Observed = mean(Observed)
+  )
+
+# anova chao1
 aof <- function(x){
-  anova(aov(x~treatment))
+  y <- anova(aov(x ~ treatment, data = combined_indices))
+  return(y)
 }
-aov.result.chao1 <- apply(combined_indices$Chao1, 1, aof)
-boxplot(Chao1~treatment, data = combined_indices)
 
-anova <- aov.result %>% 
-  anova(aov.result) %>%
-  ggplot(aes(x = treatment, y = anova, fill = treatment))
+aof.plot <- function(x,y){
+  plot <- ggplot(combined_indices, aes(x = treatment, y = x, fill = treatment)) +
+  geom_boxplot() +
+    # facet_wrap( ~ time, ncol = 2) +
+    theme_classic() + 
+    ylab(colnames(combined_indices[y])) +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) 
+  return(plot)
+}
 
-         
+anova_chao1 <- aof(combined_indices$Chao1)
+
+aof.plot(combined_indices$Chao1,3)
+
+# anova shannon
+anova_shannon <- aof(combined_indices$Shannon)
+aof.plot(combined_indices$Shannon,4)
+
+# anova simpson
+anova_simpson <- aof(combined_indices$Simpson)
+aof.plot(combined_indices$Simpson,5)
+
+# anova 
+anova_observed <- aof(combined_indices$Observed)
+aof.plot(combined_indices$Observed, 6)
+
