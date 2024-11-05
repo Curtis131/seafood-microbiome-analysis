8edd046ac501ee6a15e67490d59b3943a628c142
Curtis <yishuo.zhang@mail.mcgill.ca>
2024-11-05 15:33:11 -0500

    coho corrlation analysis

--- corrlation analysis/corrlation_anlysis_sockeye.R
+++ corrlation analysis/corrlation_anlysis_sockeye.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(corrplot)
 require(remotes)
 library(Hmisc)
-
+## sockeye
 data <- read.csv("/home/zys/Downloads/abundance_data/abundance_sockeye_corr.csv",
                  as.is = TRUE)
 data_sockeye <- data %>% 
@@ -24,6 +24,36 @@ res.microbe <- rcorr(as.matrix(microbes))
 corplot.matrix <- res$r[1:11,-c(1:11)]
 
 corrplot(corplot.matrix, diag = FALSE,
-         tl.cex = 1,
-         title = "sockeye corrlations")
+         tl.cex = 0.8,
+         mar = c(1,1,1,1))
+corrplot(res.microbe$r, diag = FALSE,
+         tl.cex = 0.8,
+         mar = c(1,1,1,1))
 
+### coho
+data_coho <- read.csv("/home/zys/Documents/seafood-microbiome-analysis/corrlation analysis/abundance_data/abundance_corr_coho.csv",
+                      as.is = TRUE)
+data_coho <- data_coho %>%
+  filter_all(any_vars(!is.na(.))) %>%
+  select_if(~ any(!is.na(.)))
+microbes_coho <- data_coho %>%
+  select(c(Day,Bradyrhizobium, Brochothrix, Carnobacterium, Clostridium.sensu.stricto.1,
+           Cupriavidus, Latilactobacillus, Leuconostoc, Photobacterium, Pseudomonas,
+           Yersinia))
+chem_prop_coho <- data_coho %>%
+  select(-c(Day,Bradyrhizobium, Brochothrix, Carnobacterium, Clostridium.sensu.stricto.1,
+           Cupriavidus, Latilactobacillus, Leuconostoc, Photobacterium, Pseudomonas,
+           Yersinia))
+res <- rcorr(as.matrix(microbes_coho), as.matrix(chem_prop_coho), type = "spearman")
+res.microbe <- rcorr(as.matrix(microbes_coho))
+
+corplot.matrix <- res$r[1:11,-c(1:11)]
+
+corrplot(corplot.matrix, diag = FALSE,
+         tl.cex = 0.8,
+         mar = c(1,1,1,1))
+corrplot(res.microbe$r, diag = FALSE,
+         tl.cex = 0.8,
+         mar = c(1,1,1,1))
+
+### 
