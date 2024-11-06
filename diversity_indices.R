4477cc8f2a64e0df08672d0f57f559161268f2be
Curtis <yishuo.zhang@mail.mcgill.ca>
2024-11-05 19:03:31 -0500

    completed salmon analysis

--- diversity_indices.R
+++ diversity_indices.R
@@ -71,7 +71,7 @@ shrimp.genus.df <- shrimp.genus.df %>%
 shrimp.genus.df$Genus <- as.factor(shrimp.genus.df$Genus)
 shrimp.genus.df.ag <- shrimp.genus.df %>%
   group_by(Sample, Genus,day,treatment) %>%
-  summarize(total_abundance = sum(Abundance), .groups = "drop") %>%
+  summarise(total_abundance = sum(Abundance), .groups = "drop") %>%
   group_by(Sample) %>%
   mutate(total_sample_abundance = sum(total_abundance)) %>%
   ungroup() %>%
@@ -80,7 +80,7 @@ shrimp.genus.df.ag <- shrimp.genus.df %>%
   filter(relative_abundance > 0) %>%
   mutate(Genus = ifelse(relative_abundance < 0.01, "others", as.character(Genus))) %>%
   group_by(Sample, Genus,treatment,day) %>%
-  summarize(total_abundance = sum(total_abundance), 
+  summarise(total_abundance = sum(total_abundance), 
             relative_abundance = sum(relative_abundance), 
             .groups = 'drop') %>%
   mutate(treatment = factor(treatment))
@@ -89,6 +89,133 @@ shrimp.genus.df.ag$day <- factor(shrimp.genus.df.ag$day,
 shrimp.genus.df.ag <- arrange(shrimp.genus.df.ag,shrimp.genus.df.ag$day)
 shrimp.genus.df.ag$Sample <- factor(shrimp.genus.df.ag$Sample, 
                                           levels = unique(shrimp.genus.df.ag$Sample[order(shrimp.genus.df.ag$day)]))
+
+## salmon ##
+salmon <- prune_taxa(taxa_sums(salmon) > 0, salmon)
+salmon.genus <- tax_glom(salmon, taxrank = "Genus")
+salmon.genus.df <- psmelt(salmon.genus)
+salmon.genus.df <- salmon.genus.df %>%
+  mutate(relative_abundance = sapply(Abundance, function(x) x/sum(Abundance)))
+salmon.genus.df$Genus <- as.factor(salmon.genus.df$Genus)
+salmon.genus.df.ag <- salmon.genus.df %>%
+  group_by(Sample, Genus,day,treatment) %>%
+  dplyr::summarise(total_abundance = sum(Abundance), .groups = "drop") %>%
+  group_by(Sample) %>%
+  mutate(total_sample_abundance = sum(total_abundance)) %>%
+  ungroup() %>%
+  mutate(relative_abundance = total_abundance / total_sample_abundance) %>%
+  select(Sample, day, treatment, Genus,total_abundance, relative_abundance) %>%
+  filter(relative_abundance > 0) %>%
+  mutate(Genus = ifelse(relative_abundance < 0.01, "others", as.character(Genus))) %>%
+  group_by(Sample, Genus,treatment,day) %>%
+  summarise(total_abundance = sum(total_abundance), 
+            relative_abundance = sum(relative_abundance), 
+            .groups = 'drop') %>%
+  mutate(treatment = factor(treatment))
+salmon.genus.df.ag$day <- factor(salmon.genus.df.ag$day,
+                                 levels = c(0,3,6,12,15))
+salmon.genus.df.ag <- arrange(salmon.genus.df.ag,salmon.genus.df.ag$day)
+salmon.genus.df.ag$Sample <- factor(salmon.genus.df.ag$Sample, 
+                                    levels = unique(salmon.genus.df.ag$Sample[order(salmon.genus.df.ag$day)]))
+
+ggplot(data = salmon.genus.df.ag %>% filter(day == 6),
+       aes(x = Sample, y = relative_abundance, fill = Genus))+
+  geom_bar(aes(),stat = "identity", position = "stack") +
+  facet_grid(treatment ~ ., scales = "free_y") +
+  coord_flip()
+
+chao1_salmon <- estimate_richness(salmon, measures = "Chao1")
+shannon_salmon <- estimate_richness(salmon, measures = "Shannon")
+simpson_salmon <- estimate_richness(salmon, measures = "Simpson")
+observed_salmon <- estimate_richness(salmon, measures = "Observed")
+
+combined_indices_salmon <- data.frame(Sample = rownames(shannon_salmon),
+                                      Chao1 = chao1_salmon$Chao1,
+                                      Shannon = shannon_salmon$Shannon,
+                                      Simpson = simpson_salmon$Simpson, 
+                                      Observed = observed_salmon$Observed,
+                                      day = salmon@sam_data$day,
+                                      replicate = salmon@sam_data$replicate,
+                                      treatment = salmon@sam_data$treatment)
+salmon@sam_data$day <- factor(salmon@sam_data$day,
+                              levels = c(0,3,6,12,15))
+
+plot_richness(salmon, x = "day", measures = "Shannon",color = "treatment") +
+  geom_boxplot() +
+  ylab("Shannon")
+
+plot_richness(salmon, x = "day", measures = "Observed", color = "treatment") +
+  geom_boxplot() +
+  ylab("Observed")
+
+plot_richness(salmon, x = "day", measures = "Simpson", color = "treatment") +
+  ylab("Simpson") +
+  geom_boxplot()
+
+plot_richness(salmon, x = "day", measures = "Chao1", color = "treatment") +
+  ylab("Chao1") +
+  geom_boxplot()
+
+combined_indices_salmon_trans <- combined_indices_salmon %>%
+  group_by(day,treatment) %>%
+  summarise(
+    Chao1 = mean(Chao1),
+    Shannon = mean(Shannon),
+    Simpson = mean(Simpson),
+    Observed = mean(Observed)
+  ) %>%
+  mutate(log(Chao1),
+         log(Shannon),
+         log(Simpson),
+         log(Observed)
+  )
+
+combined_indices_salmon_trans <- combined_indices_salmon_trans %>%
+  mutate(`log(Shannon)` = `log(Shannon)` + 0.3954230,
+         `log(Simpson)` = `log(Simpson)` + 1.24222572)
+
+aof.salmon <- function(x){
+  y <- anova(aov(x ~ as.factor(day) + treatment, data = combined_indices_salmon_trans))
+  return(y)
+}
+
+bc.salmon <- as.matrix(distance(salmon, method = "bray"))
+pcoa_bc_salmon <- ordinate(salmon, "PCoA", "bray")
+plot_ordination(salmon,pcoa_bc_salmon, color = "day", shape = "treatment")
+# permutation ANOVA test
+permanova_salmon<- adonis2(bc.salmon ~ day+treatment, data = salmonexp, permutations = 999)
+bc.salmon.tibble <- bc.salmon %>%
+  as.matrix() %>%
+  as_tibble(rownames = "sample") %>%
+  pivot_longer(-sample) %>%
+  filter(sample < name) %>%
+  mutate(time = as.numeric(
+    sapply(strsplit(sample,"-"),'[',2)
+  )) %>%
+  mutate(treatment = as.vector(
+    sapply(strsplit(sample, "-"), '[',1)
+  )) %>%
+  mutate(treatment = case_when(
+    treatment == "A" ~ "control",
+    treatment == "B" ~ "Nisin",
+    treatment == "C" ~ "Pediocin",
+    treatment == "D" ~ "Divergicin", 
+    treatment == "E" ~ "MicrocinJ25"
+  )) %>%
+  group_by(time, sample, treatment) %>%
+  summarise(median = median(value))%>%
+  ungroup()
+
+bc.salmon.tibble$replicate <- substr(bc.salmon.tibble$sample,
+                                     nchar(bc.salmon.tibble$sample) - 2,
+                                     nchar(bc.salmon.tibble$sample))
+
+# plot
+ggplot(data = bc.salmon.tibble, aes(x=time, y=median, color = treatment,
+                                    group = paste0(replicate,treatment))) +
+                                   
+  geom_line(size = 0.25)+
+  geom_smooth(aes(group = treatment), size = 3)
 
 ######## abundance  plot ###########
 ggplot(data = shrimp.genus.df.ag,
@@ -128,19 +255,19 @@ combined_indices_shrimp <- data.frame(Sample = rownames(shannon_shrimp),
 
 shrimp@sam_data$day <- factor(shrimp@sam_data$day,
                               levels = c(0,2,4,6,8,10))
-plot_richness(shrimp, x = "day", measures = "Shannon") +
+plot_richness(salmon, x = "treatment", measures = "Shannon") +
   geom_boxplot() +
   ylab("Shannon")
 
-plot_richness(shrimp, x = "day", measures = "Observed") +
+plot_richness(salmon, x = "treatment", measures = "Observed") +
   geom_boxplot() +
   ylab("Observed")
 
-plot_richness(shrimp, x = "day", measures = "Simpson") +
+plot_richness(salmon, x = "treatment", measures = "Simpson") +
   ylab("Simpson") +
   geom_boxplot()
 
-plot_richness(shrimp, x = "day", measures = "Chao1") +
+plot_richness(salmon, x = "treatment", measures = "Chao1") +
   ylab("Chao1") +
   geom_boxplot()
 
